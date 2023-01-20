cae2cad0d3fed5a2e1ea4037b8e8a3a0696788e9
W3llz1n <wellintoh.silva.brito@gmail.com>
2023-01-19 21:47:45 -0300

    Commit inicial

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+import HelloWorld from './component/HelloWorld';
+
+function App() {
+  return (
+    <div className="App">
+
+      <header className="App-header">
+
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <HelloWorld
+          text='Hello World!!!'
+        />
+
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
