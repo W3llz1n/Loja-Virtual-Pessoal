d72a020bb1ed4858eaae01994e2d1f8684b2e910
W3llz1n <wellintoh.silva.brito@gmail.com>
2023-01-19 22:01:59 -0300

    Adicionando pages de teste

---
+++ src/pages/catálogos/catalogos.js
@@ -0,0 +1,11 @@
+function Catalogos() {
+
+    return (
+        <div>
+            <h1>Catalogos</h1>
+            <p>Conteúdo de pagina.</p>
+        </div>
+    )
+}
+
+export default Catalogos
---
+++ src/pages/home/Home.js
@@ -0,0 +1,11 @@
+function Home(){
+
+    return(
+        <div>
+            <h1>Home</h1>
+            <p>Conteúdo de pagina.</p>
+        </div>
+    )
+}
+
+export default Home
